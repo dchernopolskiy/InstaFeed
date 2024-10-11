932b7d903bed86f7d55095c2aca3ed8a00b2ce0c
dchernopolskiy <58235192+dchernopolskiy@users.noreply.github.com>
2024-10-10 19:57:16 -0700

    Initial Commit

---
+++ InstaFeedTests/InstaFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFeedTests.swift
+//  InstaFeedTests
+//
+//  Created by Dan Chernopolskii on 10/10/24.
+//
+
+import Testing
+@testable import InstaFeed
+
+struct InstaFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
